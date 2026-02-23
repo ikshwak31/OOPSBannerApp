b9998385455054765f9aa29b8579b2ecad2116ec
ikshwak31 <br4915@srmist.edu.in>
2026-02-23 13:31:11 +0530

    UC4: Used String Array and enhanced for-loop to print banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,47 +1,27 @@
+/**
+* OOPSBannerApp UC4 - String Array and Loop
+ * This version stores the banner lines in a String array 
+ * and uses an enhanced for-loop to print them, reducing repetitive code.
+ *
+ * @author Developer
+ * @version 4.0
+ */
 public class OOPSBannerApp {
-
     public static void main(String[] args) {
 
-        System.out.println(String.join(" ",
-                " ***  ",
-                " ***  ",
-                " **** ",
-                " *****"));
+        String[] banner = new String[7];
 
-        System.out.println(String.join(" ",
-                "**   **",
-                "**   **",
-                "**   **",
-                "**     "));
+        banner[0] = String.join(" ", "  *****  ", "  *****  ", " ******  ", "  *****  ");
+        banner[1] = String.join(" ", " *     * ", " *     * ", " *     * ", " *     * ");
+        banner[2] = String.join(" ", " *     * ", " *     * ", " *     * ", " *       ");
+        banner[3] = String.join(" ", " *     * ", " *     * ", " ******  ", "  *****  ");
+        banner[4] = String.join(" ", " *     * ", " *     * ", " *       ", "       * ");
+        banner[5] = String.join(" ", " *     * ", " *     * ", " *       ", " *     * ");
+        banner[6] = String.join(" ", "  *****  ", "  *****  ", " *       ", "  *****  ");
 
-        System.out.println(String.join(" ",
-                "**   **",
-                "**   **",
-                "****  ",
-                " **** "));
-
-        System.out.println(String.join(" ",
-                "**   **",
-                "**   **",
-                "**     ",
-                "     **"));
-
-        System.out.println(String.join(" ",
-                "**   **",
-                "**   **",
-                "**     ",
-                "**   **"));
-
-        System.out.println(String.join(" ",
-                "**   **",
-                "**   **",
-                "**     ",
-                "**   **"));
-
-        System.out.println(String.join(" ",
-                " ***  ",
-                " ***  ",
-                "**     ",
-                " **** "));
+        // Step 3: Use enhanced for-loop to print
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
